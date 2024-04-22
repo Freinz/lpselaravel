b2dd57a4eaa037764a5ba7ca9edf4d3c89e1b1fb
Freinz <toriqthalib94@gmail.com>
2024-04-22 20:27:00 +0800

    intall and configuration template laravel

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Auth::routes();
+
+
+// Define a group of routes with 'auth' middleware applied
+Route::middleware(['auth'])->group(function () {
+    // Define a GET route for the root URL ('/')
+    Route::get('/', function () {
+        // Return a view named 'index' when accessing the root URL
+        return view('index');
+    });
+
+    // Define a GET route with dynamic placeholders for route parameters
+    Route::get('{routeName}/{name?}', [HomeController::class, 'pageView']);
+});
+
+
+Route::get('/add_user', [UserController::class,'add_user']);
+
+Route::get('/show_user', [UserController::class,'show_user']);
